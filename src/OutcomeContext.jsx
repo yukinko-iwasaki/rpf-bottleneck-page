a3cd8ac09ab16c81345dbc82c870c51b843208e2
Yukiko Suzuki <yukinko.iwasaki@gmail.com>
2025-07-25 14:50:06 -0400

    Update OutcomeContext.jsx

--- src/OutcomeContext.jsx
+++ src/OutcomeContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react';
 const OutcomeContext = createContext();
 
 export const OutcomeProvider = ({ children }) => {
-  const [outcome, setOutcome] = useState('Education');
+  const [outcome, setOutcome] = useState('');
 
   return (
     <OutcomeContext.Provider value={{ outcome, setOutcome }}>
